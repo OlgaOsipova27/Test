fe17e9d970998e1cb366174ac7474974d4ef34f9
OlgaOsipova27 <opetrova0498@gmail.com>
2025-07-28 11:47:02 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+URL_SERVICE = "https://6738ee54-5029-4613-8a9f-aaef76476f46.serverhub.praktikum-services.ru"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" 
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
